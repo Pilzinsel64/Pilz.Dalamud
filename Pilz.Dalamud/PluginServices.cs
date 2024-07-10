e699a758f3822394df8a2a283d55e23085ac21de
Pilzinsel64 <pilzinsel64@gmx.de>
2024-07-10 11:13:09 +0200

    use IDatamudPluginInterface

--- Pilz.Dalamud/PluginServices.cs
+++ Pilz.Dalamud/PluginServices.cs
@@ -15,14 +15,14 @@ namespace Pilz.Dalamud
 {
     public class PluginServices
     {
-        [PluginService] public static DalamudPluginInterface PluginInterface { get; set; } = null;
+        [PluginService] public static IDalamudPluginInterface PluginInterface { get; set; } = null;
         [PluginService] public static IGameGui GameGui { get; set; } = null;
         [PluginService] public static IClientState ClientState { get; set; } = null;
         [PluginService] public static IDataManager DataManager { get; set; } = null;
         [PluginService] public static IObjectTable ObjectTable { get; set; } = null;
         [PluginService] public static IGameInteropProvider GameInteropProvider { get; set; } = null;
 
-        public static void Initialize(DalamudPluginInterface dalamudPluginInterface)
+        public static void Initialize(IDalamudPluginInterface dalamudPluginInterface)
         {
             dalamudPluginInterface.Create<PluginServices>();
         }
